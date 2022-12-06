bdb4a707244d784c23b5a6ddefab816fdf1630a8
JuanPabl0770 <53346852+JuanPabl0770@users.noreply.github.com>
2022-12-06 10:47:19 -0500

    minor fix to day 06 solution

--- day06/day06.py
+++ day06/day06.py
@@ -1,13 +1,14 @@
 class DeviceFixer:
     def get_start_of_packet(self, datastream, num_diff_chars):
         for i in range(len(datastream)):
-            if i >= 4 and len({datastream[i] for i in range(i, i - num_diff_chars, -1)}) == num_diff_chars:
+            if i >= num_diff_chars and len({datastream[i] for i in range(i, i - num_diff_chars, -1)}) == num_diff_chars:
                 return i + 1, {datastream[i] for i in range(i, i - num_diff_chars, -1)}
         return None
+
 
 f = open('input.txt', 'r')
 datastream = f.readline()
 deviceFixer = DeviceFixer()
-print(deviceFixer.get_start_of_packet(datastream.strip(),4))
-print(deviceFixer.get_start_of_packet(datastream.strip(),14))
+print(deviceFixer.get_start_of_packet(datastream.strip(), 4))
+print(deviceFixer.get_start_of_packet(datastream.strip(), 14))
 f.close()
